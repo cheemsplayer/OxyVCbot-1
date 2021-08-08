b397d9ca21e197d9ab05ac3ba2fd5c745c2c2cd6
cheemsplayer <85833852+cheemsplayer@users.noreply.github.com>
2021-08-08 18:30:04 +0530

    Update play.py

--- OxyXmusic/modules/play.py
+++ OxyXmusic/modules/play.py
@@ -1065,7 +1065,7 @@ async def jiosaavn(client: Client, message_: Message):
                     # print(e)
                     await lel.edit(
                         f"<b>🔴 Flood Wait Error 🔴 \nUser {user.first_name} couldn't join your group due to heavy requests for userbot! Make sure user is not banned in group."
-                        "\n\nOr manually add @Cheem_sVC_assistant to your Group and try again</b>",
+                        "\n\nOr manually add @WonderGirl_sAssistant to your Group and try again</b>",
                     )
     try:
         await USER.get_chat(chid)
